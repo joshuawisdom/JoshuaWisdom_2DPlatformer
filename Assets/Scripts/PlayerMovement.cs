69a7d9f8fe0ad97daac13748482320b2537180bb
joshuawisdom <78412743+joshuawisdom@users.noreply.github.com>
2021-04-04 20:18:50 -0700

    Updated ReadMe and Scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,16 @@
-﻿using System.Collections;
+﻿// Joshua Wisdom
+// 2313991
+// CPSC 236-03
+// jowisdom@chapman.edu
+// Project 05: 2D Platformer
+// This is my own work, and I did not cheat on this assignment.
+
+// No known errors.
+
+// Completed alongside provided videos with provided materials.
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # JoshuaWisdom_2DPlatformer
  
+ Joshua Wisdom
+ 2313991
+ CPSC 236-03
+ jowisdom@chapman.edu
+ Project 05: 2D Platformer
+ This is my own work, and I did not cheat on this assignment.
+
+ No known errors.
+
+ Completed alongside provided videos with provided materials.
